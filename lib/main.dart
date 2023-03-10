3b8631b0d849df7c173f4cfd4056376f9631f6da
Jayesh <gujarjayesh446@gmail.com>
2023-03-10 16:25:23 +0530

    done

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+// Dart imports:
+
+import 'package:flutter/material.dart';
+import 'package:live_streaming_cohost/home_page.dart';
+
+// Package imports:
+import 'package:zego_uikit_prebuilt_live_streaming/zego_uikit_prebuilt_live_streaming.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  ZegoUIKit().initLog().then((value) {
+    runApp(const MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flutter Demo', home: HomePage());
+  }
+}
